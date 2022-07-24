b49ae98d53579df5674f1946a7a9b0affc2a8152
samuchakraborty <samuckb@gmail.com>
2022-07-24 20:27:59 +0600

    html redirect complete

---
+++ algorithm/merge_sort.go
@@ -0,0 +1,24 @@
+package algorithm
+
+import "fmt"
+
+// import (
+// 	"time"
+// )
+
+func MergeSort(arr []int, lowerbound int, uperbound int ) ([]int, ) {
+fmt.Printf("%v %v %v\n", arr, lowerbound, uperbound)
+
+	// for i := 1; i < len(arr); i++ {
+	// 	j := i
+	// 	for j > 0 {
+	// 		if arr[j-1] > arr[j] {
+	// 			arr[j], arr[j-1] = arr[j-1], arr[j]
+	// 		}
+	// 		j--
+	// 	}
+
+	// }
+	
+	return arr
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"h/algorithm"
 	"h/utils"
 	"math/rand"
@@ -13,91 +12,106 @@ import (
 )
 
 func main() {
-
-	//app := fiber.New()
-	//app.Static("", "./index.html")
 	engine := html.New("./views", ".html")
 
 	app := fiber.New(fiber.Config{Views: engine})
 
+
+	arr := []int{90,40,1,5,6,7}
+
+	algorithm.MergeSort(arr, arr[0], arr[len(arr)-1])
+	
 	app.Static("./style", "./styles/style")
-	app.Get("/index", func(c *fiber.Ctx) error {
-
-		input := (c.Query("number"))
-		number, err := strconv.Atoi(input)
-		if err != nil {
-			panic(err)
-		}
-		rand.Seed(time.Now().UnixNano())
-
-		array := []int{}
-		for i := 0; i < number; i++ {
-			round := rand.Intn(100)
-			array = append(array, round)
-
-		}
-
-		result, executionTime := algorithm.BubbleSort(array)
-		insertionSort, executionTime2 := algorithm.InsertionSort(array)
-
-		insertionSortData := utils.InsertionSort{
-			Data:          insertionSort,
-			ExecutionTime: executionTime2,
-		}
-		bubbleSortData := utils.BubbleSort{
-			Data:          result,
-			ExecutionTime: executionTime,
-		}
-
-		util := &utils.Data{
-			Message:       "success",
-			StatusCode:    200,
-			BubbleSort:    bubbleSortData,
-			InsertionSort: insertionSortData,
-		}
-
-		// return c.JSON(response)
-
-		return c.Render("index", fiber.Map{
-
-			"msg":                 util.Message,
-			"bubble_sort_time":    util.BubbleSort.ExecutionTime,
-			"insertion_sort_time": util.InsertionSort.ExecutionTime,
-			"data":                util.BubbleSort.Data,
-		})
-
-	})
-
-	// app.Get("/", func(c *fiber.Ctx) error {
-
-	// 	return c.Render("index", fiber.Map{
-
-	// 		"msg": "samu",
-	// 	})
-	// })
-
 	app.Post("", func(c *fiber.Ctx) error {
 
-		payLoad := utils.BodyData{}
-		if err := c.BodyParser(&payLoad); err != nil {
-			return err
-		}
-		fmt.Println(payLoad)
-
-		return c.SendString("post work")
-	})
-	app.Post("/post", func(c *fiber.Ctx) error {
 		payload := struct {
-			Name  string `json:"name"`
-			Email string `json:"email"`
+			Number string `json:"number"`
 		}{}
 
 		if err := c.BodyParser(&payload); err != nil {
 			return err
 		}
-		fmt.Print(payload)
-		return c.JSON(payload)
+
+		// fmt.Println(payload)
+
+		// input := (c.Query("number"))
+		// n := payload.Number(int)
+		if payload.Number != "" {
+			number, err := strconv.Atoi(payload.Number)
+			if err != nil {
+				panic(err)
+			}
+			rand.Seed(time.Now().UnixNano())
+
+			array := []int{}
+			for i := 0; i < number; i++ {
+				round := rand.Intn(100)
+				array = append(array, round)
+
+			}
+
+			result, executionTime := algorithm.BubbleSort(array)
+			insertionSort, executionTime2 := algorithm.InsertionSort(array)
+
+			insertionSortData := utils.InsertionSort{
+				Data:          insertionSort,
+				ExecutionTime: executionTime2 * time.Millisecond,
+			}
+			bubbleSortData := utils.BubbleSort{
+				Data:          result,
+				ExecutionTime: executionTime * time.Millisecond,
+			}
+
+			util := &utils.Data{
+				Message:       "success",
+				StatusCode:    200,
+				BubbleSort:    bubbleSortData,
+				InsertionSort: insertionSortData,
+			}
+
+			// return c.JSON(response)
+
+			return c.Render("index", fiber.Map{
+
+				"msg":                 util.Message,
+				"bubble_sort_time":    int64(util.BubbleSort.ExecutionTime / time.Millisecond),
+				"insertion_sort_time": int64(util.InsertionSort.ExecutionTime / time.Millisecond),
+				"data":                util.BubbleSort.Data,
+			})
+		} else {
+			return c.Render("index", fiber.Map{})
+
+		}
+
 	})
+
+	app.Get("", func(c *fiber.Ctx) error {
+
+		return c.Render("index", fiber.Map{})
+	})
+
+	// app.Post("", func(c *fiber.Ctx) error {
+
+	// 	payLoad := utils.BodyData{}
+	// 	if err := c.BodyParser(&payLoad); err != nil {
+	// 		return err
+	// 	}
+	// 	fmt.Println(payLoad)
+
+	// 	return c.SendString("post work")
+	// })
+	// app.Post("", func(c *fiber.Ctx) error {
+	// 	payload := struct {
+	// 		Number string `json:"number"`
+	// 	}{}
+
+	// 	if err := c.BodyParser(&payload); err != nil {
+	// 		return err
+	// 	}
+	// 	fmt.Print(payload)
+	// 	// return c.JSON(payload)
+	// 	return c.Redirect("index")
+	// })
 
 	app.Listen(":3000")
 	// fmt.Printf("%v \n", result)
